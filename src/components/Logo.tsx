3548445e6a9788375c206f1bd59350f61f949107
Brandboostworks <brandboostworks@gmail.com>
2024-12-30 13:45:20 +0530

    B

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../../
 import { Rocket } from 'lucide-react';
 
 const Logo = () => {
   return (
     <div className="flex items-center gap-2">
-      <Rocket className="w-6 h-6" />
-      <span className="text-xl font-bold">BrandBoost</span>
+      <img src={LogoSVG} alt="BrandBoost Logo" className="w-10 h-10 " />
+     
     </div>
   );
 };
